5d26d2eb08648b85f48094bd7c353e273415afbf
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-03-11 21:34:20 +0200

    task completed

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+}));
+function App() {
+  return <div></div>;
+}
+export default App;
